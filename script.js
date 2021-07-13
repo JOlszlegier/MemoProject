8e04af9983f196812ee98b726f72b042d26612da
kolszlegier41 <kolszlegier41@gmail.com>
2021-07-13 11:16:09 +0200

    mobile

--- script.js
+++ script.js
@@ -8,68 +8,134 @@ let namesDeck = []
 const deckArray = [
         {
             name:'dog',
-            img: 'Dog100.jpg'
+            img: 'pictures/Dog100.jpg'
         },
         {
             name:'dog',
-            img: 'Dog100.jpg'
+            img: 'pictures/Dog100.jpg'
         },
         {
             name:'bug',
-            img: 'Bug100.jpg'
+            img: 'pictures/Bug100.jpg'
         },
         {
             name:'bug',
-            img: 'Bug100.jpg'
+            img: 'pictures/Bug100.jpg'
         },
         {
             name:'parrots',
-            img: 'Parrots100.jpg'
+            img: 'pictures/Parrots100.jpg'
         },
         {
             name:'parrots',
-            img: 'Parrots100.jpg'
+            img: 'pictures/Parrots100.jpg'
         },
         {
             name:'dino',
-            img: 'dino100.jpg'
+            img: 'pictures/dino100.jpg'
         },
         {
             name:'dino',
-            img: 'dino100.jpg'
+            img: 'pictures/dino100.jpg'
         },
         {
             name:'frog',
-            img: 'Frog100.jpg'
+            img: 'pictures/Frog100.jpg'
         },
         {
             name:'frog',
-            img: 'Frog100.jpg'
+            img: 'pictures/Frog100.jpg'
         },
         {
             name:'koala',
-            img: 'koala100.jpg'
+            img: 'pictures/koala100.jpg'
         },
         {
             name:'koala',
-            img: 'koala100.jpg'
+            img: 'pictures/koala100.jpg'
         },
         {
             name:'pig',
-            img: 'Pig100.jpg'
+            img: 'pictures/Pig100.jpg'
         },
         {
             name:'pig',
-            img: 'Pig100.jpg'
+            img: 'pictures/Pig100.jpg'
         },
         {
             name:'howl',
-            img: 'Howl100.jpg'
+            img: 'pictures/Howl100.jpg'
         },
         {
             name:'howl',
-            img: 'Howl100.jpg'
+            img: 'pictures/Howl100.jpg'
         },
+]
+const deckArrayMobile = [
+    {
+        name:'dog',
+        img: 'pictures/DogSmall.jpg'
+    },
+    {
+        name:'dog',
+        img: 'pictures/DogSmall.jpg'
+    },
+    {
+        name:'bug',
+        img: 'pictures/BugSmall.jpg'
+    },
+    {
+        name:'bug',
+        img: 'pictures/BugSmall.jpg'
+    },
+    {
+        name:'parrots',
+        img: 'pictures/ParrotsSmall.jpg'
+    },
+    {
+        name:'parrots',
+        img: 'pictures/ParrotsSmall.jpg'
+    },
+    {
+        name:'dino',
+        img: 'pictures/dinoSmall.jpg'
+    },
+    {
+        name:'dino',
+        img: 'pictures/dinoSmall.jpg'
+    },
+    {
+        name:'frog',
+        img: 'pictures/FrogSmall.jpg'
+    },
+    {
+        name:'frog',
+        img: 'pictures/FrogSmall.jpg'
+    },
+    {
+        name:'koala',
+        img: 'pictures/koalaSmall.jpg'
+    },
+    {
+        name:'koala',
+        img: 'pictures/koalaSmall.jpg'
+    },
+    {
+        name:'pig',
+        img: 'pictures/PigSmall.jpg'
+    },
+    {
+        name:'pig',
+        img: 'pictures/PigSmall.jpg'
+    },
+    {
+        name:'howl',
+        img: 'pictures/HowlSmall.jpg'
+    },
+    {
+        name:'howl',
+        img: 'pictures/HowlSmall.jpg'
+    },
 ]
 
 const shuffledDeck = (array) => {
@@ -89,20 +155,27 @@ const variableErasing = () =>{
     doneCards =[];
     clicks = 0;
 }
-const deckMapping = () =>{
-    namesDeck = deckArray.map((cards) => {return cards.name});
+
+const deckMapping = (array) =>{
+    namesDeck = array.map((cards) => {return cards.name});
 }
 
 const attributeSetting = (card,i) =>{
-    card.setAttribute('src','tile.jpg')
+    card.setAttribute('src','pictures/tile.jpg')
     card.setAttribute('id',i.toString());
     card.setAttribute('class','single-box');
 }
 
-const cardListener = (card,i) =>{
+const attributeSettingMobile = (card,i) =>{
+    card.setAttribute('src','pictures/tileSmall.jpg')
+    card.setAttribute('id',i.toString());
+    card.setAttribute('class','single-box');
+}
+
+const cardListener = (card,i,array) =>{
     card.addEventListener('click', e =>{
         clickCounter();
-        card.setAttribute('src',deckArray[i].img)
+        card.setAttribute('src',array[i].img)
         clickedCards.push(namesDeck[i]);
         clickedCardsId.push(i);
         if(clickedCards.length === 2) {
@@ -111,21 +184,27 @@ const cardListener = (card,i) =>{
     })
 }
 
+
+
 const scoreCheck = () =>{
+    let picture = 0;
+    if (mobile.matches) picture= 'pictures/tileSmall.jpg';
+    else picture = 'pictures/tile.jpg';
+
     const cards = document.querySelectorAll('img');
     if(clickedCards[0] === clickedCards[1]) {
         doneCards.push(clickedCards[0]);
         doneCards.push(clickedCards[0]);
     }
     else if(doneCards.find(element => element===clickedCards[0])){
-        cards[clickedCardsId[1]].setAttribute('src','tile.jpg');
+        cards[clickedCardsId[1]].setAttribute('src',picture);
     }
     else if (doneCards.find(element => element===clickedCards[1])){
-        cards[clickedCardsId[0]].setAttribute('src','tile.jpg');
+        cards[clickedCardsId[0]].setAttribute('src',picture);
     }
     else {
-        cards[clickedCardsId[0]].setAttribute('src','tile.jpg');
-        cards[clickedCardsId[1]].setAttribute('src','tile.jpg');
+        cards[clickedCardsId[0]].setAttribute('src',picture);
+        cards[clickedCardsId[1]].setAttribute('src',picture);
     }
     arrayErase();
     if(doneCards.length === deckArray.length){
@@ -134,11 +213,12 @@ const scoreCheck = () =>{
     }
 }
 
-const createDeck = () => {
+const createDeck = (array,mobile) => {
     for(let i=0;i<namesDeck.length;i++) {
         const card = document.createElement('img');
-        attributeSetting(card,i);
-        cardListener(card,i);
+        if (mobile === 1) attributeSettingMobile(card,i);
+        else   attributeSetting(card,i);
+        cardListener(card,i,array);
         box.append(card);
     }
 }
@@ -152,18 +232,36 @@ const clickCounter = () =>{
 
 const reset = () => {
     const box = document.querySelector(".game-box");
+    let deck = 0;
+    let mobileMode = 0;
+    if(mobile.matches){
+        deck = deckArrayMobile;
+        mobileMode = 1;
+    }
+    else{
+        deck = deckArray;
+        mobileMode = 0;
+    }
     while(box.firstChild){
         box.removeChild(box.firstChild);
     }
     variableErasing()
     arrayErase();
-    shuffledDeck(deckArray);
-    deckMapping()
-    createDeck()
+    shuffledDeck(deck);
+    deckMapping(deck)
+    createDeck(deck,mobileMode)
     document.getElementById("clicks").innerHTML = moves;
 }
 
-shuffledDeck(deckArray);
-deckMapping();
-createDeck();
+const mobile = window.matchMedia( "(max-width: 430px)" )
+if(mobile.matches){
+    shuffledDeck(deckArrayMobile);
+    deckMapping(deckArrayMobile);
+    createDeck(deckArrayMobile,1);
+}
+else {
+    shuffledDeck(deckArray);
+    deckMapping(deckArray);
+    createDeck(deckArray,0);
+}
 
